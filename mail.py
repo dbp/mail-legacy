ea42ce8b5bd0304b59e0adb0808a975f29905ba0
Daniel Patterson <dbp@dbpmail.net>
2013-09-05 00:34:12 -0400

    better formatting

--- mail.py
+++ mail.py
@@ -1,9 +1,9 @@
-from bottle import route, run, template
+from bottle import route, run
 from os import popen
 
 @route('/mail/')
 def index():
     f = popen("notmuch search tag:important AND tag:unread AND tag:inbox")
-    return template('<pre>{{output}}</pre>', output="\n".join(f.readlines()))
+    return "<div>%s</div>" % "</div><br/><div>".join(f.readlines())
 
 run(host='localhost', port=40000)
