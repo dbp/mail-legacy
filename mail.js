44f39993a1e14fd157bda3852ecbdabea5729548
Daniel Patterson <dbp@dbpmail.net>
2013-09-08 21:58:06 -0400

    ajaxy loading instead, better UI

---
+++ mail.js
@@ -0,0 +1,56 @@
+$(function () {
+  $.ajax("/mail/all", {
+    success: function (data) {
+      var container = $("body");
+      container.text("");
+      data.content.forEach(function (e) {
+        var link = $("<div class='message'></div>");
+        link.text(e.date_relative + " - " + e.authors + " - " + e.subject);
+        link.click(function () {
+          $(".message").removeClass("active");
+          $(".message-large").remove();
+          link.addClass("active");
+          var message = $("<div class='message-large'></div>");
+          var close = $("<div class='close'>close</div>");
+          close.click(function () {
+            message.remove();
+            link.removeClass("active");
+          });
+          $.ajax("/mail/" + e.thread, {
+            success: function (data) {
+              container.prepend(message);
+              message.append(close);
+              window.data = data.content;
+              data.content[0].forEach(function (mcontent) {
+                content = mcontent[0];
+                var section = $("<div class='section'>");
+                message.append(section);
+                var headers = content.headers;
+                var headerDiv = $("<div class='headers'>");
+                headerDiv.append("<div>From: " + headers.From + "</div>");
+                headerDiv.append("<div>Subject: " + headers.Subject + "</div>");
+                headerDiv.append("<div>Date: " + headers.Date + "</div>");
+                section.append(headerDiv);
+                var bodyContent = content.body[0].content;
+                var body;
+                if (typeof bodyContent === 'string') {
+                  body = bodyContent
+                } else {
+                  var tmp = bodyContent.filter(function (o) { return o["content-type"] == "text/plain"});
+                  if (tmp.length > 0) {
+                    body = tmp[0].content;
+                  } else {
+                    body = "Could not find message body in JSON.";
+                  }
+                }
+                var bodyDiv = $("<div class='body'>" + body.replace(/\n/g, "<br />") + "</div>");
+                section.append(bodyDiv);
+              });
+            }
+          });
+        });
+        container.append(link);
+      });
+    }
+  });
+});
--- mail.py
+++ mail.py
@@ -1,5 +1,9 @@
-from bottle import route, run
+from bottle import route, run, static_file
 from os import popen
+import json
+
+def shell(cmd):
+    return json.loads("\n".join(popen(cmd).readlines()))
 
 def add_link(line):
     w = line.split()
@@ -7,12 +11,27 @@ def add_link(line):
 
 @route('/mail/')
 def index():
-    f = popen("notmuch search tag:important AND tag:unread AND tag:inbox")
-    return "<div>%s</div>" % "</div><br/><div>".join(map(add_link, f.readlines()))
+    return """<html><head>
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type=text/javascript src='/mail/mail.js'></script>
+    <link rel="stylesheet" type="text/css" href="/mail/main.css"></link>
+    </head><body>Loading...</body></html>"""
+
+@route('/mail/mail.js')
+def js():
+    return static_file("mail.js", "")
+
+@route('/mail/main.css')
+def js():
+    return static_file("main.css", "")
+
+@route('/mail/all')
+def all():
+    return {"content": shell("notmuch search --format=json tag:important AND tag:inbox")}
 
 @route('/mail/<thread>')
 def read(thread):
-    f = popen("notmuch show thread:%s" % thread)
-    return "<div>%s</div>" % "</div><div>".join(f.readlines())
+    return {"content": shell("notmuch show --format=json thread:%s" % thread)}
 
 run(host='localhost', port=40000)
